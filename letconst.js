dbd06505bb2f7d030de65b995263023e2a900694
Mahabub Sunny <office.prosany@gmail.com>
2021-02-02 16:10:41 +0600

    Practice Const and Let

---
+++ letconst.js
@@ -0,0 +1,28 @@
+// Declaring a variable using const
+const wifey = "Meher Afroz Shabrina";
+
+// Array using const variable
+const numbers = [29, 87, 67];
+// Array er bitore perimeter chnage kora jabe ba push kora jabe, kintu full array ta k change kora jabe na
+numbers[1] = 98;
+numbers.push(90, 95, 45);
+
+// Declareing a object
+const myInfo = {
+    name: "Mahbaub Hasan Sany",
+    phone: 1629315022,
+    age: 22
+};
+
+
+// Declaring a variable using let
+let patiantName = "Korim Chacha";
+// Let value can be change any time
+patiantName = "Rohima Khala";
+
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+    console.log(i)
+}
